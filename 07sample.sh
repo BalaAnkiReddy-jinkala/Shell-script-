02a274049334f2d7b9d7d6a2e23a0cae35a71579
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-02-26 18:34:22 +0530

    commit

---
+++ 07sample.sh
@@ -0,0 +1,8 @@
+echo "hello world!"
+
+echo -e "\e[31mHello World in Red Color\e[0m"
+echo -e "\e[32mHello World in Green Color\e[0m"
+echo -e "\e[33mHello World in Yellow Color\e[0m"
+echo -e "\e[34mHello World in Blue Color\e[0m"
+echo -e "\e[35mHello World in Magenta Color\e[0m"
+echo -e "\e[36mHello World in Cyan Color\e[0m"
