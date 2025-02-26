bcd8aa93c6df019accaed6e3b956c69af4e44289
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-02-26 18:59:35 +0530

    commit

--- 10sample.sh
+++ 10sample.sh
@@ -2,7 +2,9 @@ echo "enter the age of the voter:"
 read age
 
 if [ "$age" -ge 20 ]; then
-  echo "This person is eligible for vote"
+    echo "This person is eligible for vote"
 else
-  echo "This person is not eligible for vote "
+    echo "This person is not eligible for vote "
 fi
+
+
