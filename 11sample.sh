1c7aef66ab39f889f0082c7906fd64841221c25d
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-02-27 18:31:58 +0530

    commit

--- 11sample.sh
+++ 11sample.sh
@@ -1,7 +1,7 @@
 echo "Enter the number for division:"
 read num
 
-if [ num % 3 == 0 ]; then
+if (( num % 3 == 0 )); then
   echo "This number is division by 3"
 else
   echo "this number is not division by 3"
